125ed846cccd48a7c191efbc9b90b7d75c9b3730
Nick Douma <n.douma@nekoconeko.nl>
2020-05-25 17:20:21 +0200

    Disable new DOCKER_TLS_CERTDIR and allow override

--- dind-opts.sh
+++ dind-opts.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# Disable TLS mode which was enabled by default in Docker 19
+# Allow manual override using DOCKER_TLS_CERTDIR_OVERRIDE
+export DOCKER_TLS_CERTDIR="${DOCKER_TLS_CERTDIR_OVERRIDE:-}"
+
 exec dockerd-entrypoint.sh $DOCKER_OPTS $@
